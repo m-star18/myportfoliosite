3753dadf5989e4c78693fbd38eb1ea164754a501
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-22 23:55:30 +0900

    Update name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ class Footer extends Component {
           <a href="" onClick={this.props.handleOpenModal}>About</a>
           <a href="https://twitter.com/masutango18" target="_blank">Twitter</a>
           <a href="https://github.com/m-star18" target="_blank">GitHub</a>
-          <a href="https://www.instagram.com/kokushin_/" target="_blank">Instagram</a>
+          <a href="https://www.instagram.com/ryusei__0111/" target="_blank">Instagram</a>
         </div>
-        <div className="uc-footer-logo"><cite>YI</cite></div>
+        <div className="uc-footer-logo"><cite>RI</cite></div>
         <small>&copy; 2021 Ryusei Ito</small>
       </footer>
     );
