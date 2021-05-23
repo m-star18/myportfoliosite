9b2fd81b14eb9e02c7f982ef1add47dfcf7fb917
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-23 16:05:16 +0900

    Fix bug

--- src/components/WorksCard.js
+++ src/components/WorksCard.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import '../styles/WorksCard.css';
 
 class WorksCard extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       data: [],
     };
