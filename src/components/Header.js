bf2d8cf3f12cdf0b9e9bc00bb134c3a67072a946
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-23 16:07:39 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ class Header extends Component {
   render() {
     return(
       <header className="uc-header">
-        <h1 className="uc-header-logo"><a href="/">YI</a></h1>
+        <h1 className="uc-header-logo"><a href="/">RI</a></h1>
         <div className="uc-header-menu" role="navigation">
           <a href="" onClick={this.props.handleOpenModal}>About</a>
-          <a href="mailto:mail@kokush.in">Contact</a>
+          <a href="mailto:31807@toyota.kosen-ac.jp">Contact</a>
         </div>
       </header>
     );
